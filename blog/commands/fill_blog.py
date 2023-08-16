6cf42203c46dbdfabaf11637d410275cc94ca0b9
Semen Oskolkov <oskolkovsemen@yandex.ru>
2023-08-17 00:19:42 +0600

    Changed code in file

--- blog/commands/fill_blog.py
+++ blog/commands/fill_blog.py
@@ -1,26 +1,39 @@
+import os
+from django.conf import settings
 from django.core.management import BaseCommand
 from blog.models import Blog
+from django.core.files import File
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        blog = [
+        blog_data = [
             {'title': 'Копирайтинг',
              'content': 'Полезная информация про копирайтинг',
-             'preview': ''},
+             'preview': 'media/kopirayting.jpeg'},
             {'title': 'Как правильно писать письма',
              'content': 'Полезная информация о том как правильно писать письма',
-             'preview': ''},
+             'preview': 'media/letters.jpeg'},
             {'title': 'Как помогает рассылка писем',
              'content': 'Полезная информация о том как помогает рассылка писем',
-             'preview': ''},
+             'preview': 'media/mailing.png'},
             {'title': 'Грамотность важна!',
              'content': 'Полезная информация про грамотность',
-             'preview': ''}
+             'preview': 'media/full_size_1635576888-8afbe5fe7471980ff999fff29828a375.jpg'}
         ]
 
         blog_list = []
-        for item in blog:
-            blog_list.append(Blog(**item))
+        for item in blog_data:
+            preview_path = item.pop('preview')  # Удаление 'preview' из словаря
+            blog_obj = Blog(**item)
+            
+            # Добавление изображения
+            if preview_path:
+                image_path = os.path.join(settings.MEDIA_ROOT, preview_path)
+                with open(image_path, 'rb') as f:
+                    django_file = File(f)
+                    blog_obj.preview.save(os.path.basename(image_path), django_file, save=True)
+            
+            blog_list.append(blog_obj)
 
         Blog.objects.bulk_create(blog_list)
